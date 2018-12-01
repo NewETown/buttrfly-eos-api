40fe0dd28a501271a63b7d515d7799e21d744050
Everett <newetown@hotmail.com>
2018-12-01 10:04:04 -0800

    Change accounts

--- utils/statics.js
+++ utils/statics.js
@@ -3,10 +3,10 @@ const fetch = require('node-fetch')
 const { TextDecoder, TextEncoder } = require('text-encoding')
 
 // Keys and things
-const PRIVATE_KEY = "5JQrtRQ4J7fva5vBf1qa2Ae42UbECyEixFKfanD7bfh94q4SZVp"
-const PUBLIC_KEY = `EOS7o6gmLAHXgYoRLexJiy7QjLdKc6B785Ck1j4ZMN4RQons6KrF4`
-const TABLE_CODE = `buttrflytest`
-const TABLE_SCOPE = `buttrflytest`
+const PRIVATE_KEY = "5JCztbgeF4JcB9uRPYY21sX4VzwAZ1cxzeAugp2vM47W33XmCeG"
+const PUBLIC_KEY = `EOS5SBxoggbhL9DdCXspp8JQqKxK3CvH5JYjQ5LLevoA8ev3xfdWA`
+const TABLE_CODE = `buttrfly1234`
+const TABLE_SCOPE = `buttrfly1234`
 const LOCAL = 'http://127.0.0.1:7777'
 const JUNGLE_TEST = 'https://api.jungle.alohaeos.com:443'
 const MAIN_NET = 'YOUR_BP_OF_CHOICE'
